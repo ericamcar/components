03f21303c2b829ea96fd9d5d0ae915665cee0844
Erica Carvalho <ericamcar28@gmail.com>
2021-09-21 01:40:48 +0100

    add initial expanding cards

---
+++ src/components/ExpandingCards.tsx
@@ -0,0 +1,51 @@
+const IMAGE_URL_1 =
+  "https://images.unsplash.com/photo-1569864358642-9d1684040f43?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
+
+const ExpandingCards = () => {
+  return (
+    <div className="container">
+      <div
+        className="panel active"
+        style={{
+          backgroundImage: `url(${IMAGE_URL_1})`,
+        }}
+      >
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage: `url(${IMAGE_URL_1})`,
+        }}
+      >
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage: `url(${IMAGE_URL_1})`,
+        }}
+      >
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage: `url(${IMAGE_URL_1})`,
+        }}
+      >
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage: `url(${IMAGE_URL_1})`,
+        }}
+      >
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ExpandingCards;
